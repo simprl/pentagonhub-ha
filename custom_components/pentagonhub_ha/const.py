22b8d714db7e58ff9c566d63500cc22c1b758731
simprl <39172945+simprl@users.noreply.github.com>
2026-09-11 11:19:41 +0300

    DEV-37 Release PentagonHub HA v0.4.0 (#10)

--- custom_components/pentagonhub_ha/const.py
+++ custom_components/pentagonhub_ha/const.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 DOMAIN = "pentagonhub_ha"
 NAME = "PentagonHub"
 
-INTEGRATION_VERSION = "0.3.0"
+INTEGRATION_VERSION = "0.4.0"
 PROTOCOL_VERSION = 1
 
 DEFAULT_API_BASE_URL = "https://center.pentagonhub.com/api"
